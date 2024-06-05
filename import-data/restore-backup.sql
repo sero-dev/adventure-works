f03170d951a836149601503e6fc7c3ec5bb8c6f9
Sean Rodriguez <sean.rodriguez@outlook.com>
2024-06-05 17:53:43 -0400

    Fix issue where SQL Server was not restoring the database properly.

---
+++ import-data/restore-backup.sql
@@ -0,0 +1,12 @@
+RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backups/AdventureWorks2022.bak"
+GO
+
+RESTORE DATABASE [AdventureWorks2022]
+FROM DISK = '/var/opt/mssql/backups/AdventureWorks2022.bak'
+WITH
+    MOVE 'AdventureWorks2022' TO '/var/opt/mssql/data/AdventureWorks2022_Data.mdf',
+    MOVE 'AdventureWorks2022_log' TO '/var/opt/mssql/data/AdventureWorks2022_log.ldf',
+    FILE = 1,
+    NOUNLOAD,
+    STATS = 5;
+GO
